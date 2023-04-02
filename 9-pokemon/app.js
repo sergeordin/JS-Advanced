55c31c6e66e2270a9c4a842da140a08a5a449d50
Sergei Ordin <seryogaordin@gmail.com>
2023-04-02 12:17:10 +0300

    9-pokemon

---
+++ 9-pokemon/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const request = new XMLHttpRequest();
+request.open('GET', 'https://pokeapi.co/api/v2/pokemon/ditto');
+request.send();
+
+request.addEventListener('load', function () {
+    const data = JSON.parse(this.responseText);
+    const url = data.abilities[0].ability.url;
+    console.log(url);
+
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.send();
+
+    request.addEventListener('load', function () {
+        const data = JSON.parse(this.responseText);
+        const res = data.effect_entries.find(
+            (i) => i.language.name === 'en'
+        ).effect;
+        console.log(res);
+    });
+});
