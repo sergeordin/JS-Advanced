1b33caf91fb28f86163f89ac89af4000449e8db6
Sergei Ordin <seryogaordin@gmail.com>
2023-04-03 17:34:04 +0300

    .

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
 'use strict';
-
-const request = new XMLHttpRequest();
-request.open('GET', 'https://dummyjson.com/products');
-request.send();
-
-request.addEventListener('load', function () {
-    const { products } = JSON.parse(this.responseText);
-    console.log(products);
-    const sum = products.reduce((acc, p) => acc + p.price, 0);
-    console.log(sum / products.length);
-});
