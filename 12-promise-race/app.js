1d9e872828ab61b0fb8c52907f7fbe7225d077e6
Sergei Ordin <seryogaordin@gmail.com>
2023-04-08 13:31:26 +0300

    12-promise-race

---
+++ 12-promise-race/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const promise1 = new Promise((resolve) => setTimeout(() => resolve(1), 1000));
+
+const promise2 = new Promise((resolve) => setTimeout(() => resolve(2), 2000));
+
+const promise3 = new Promise((resolve) => setTimeout(() => resolve(3), 3000));
+
+const array = [promise1, promise2, promise3];
+
+function race(arr) {
+    if (!arr || arr.length === 0) {
+        return console.log(null);
+    }
+    return Promise.race(arr).then((data) => console.log(data));
+}
+
+race(array);
