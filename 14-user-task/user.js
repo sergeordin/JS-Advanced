949ab9f71461be2052eead1619ea7244fc9d5a34
Sergei Ordin <seryogaordin@gmail.com>
2023-04-11 20:23:33 +0300

    фикс

--- 14-user-task/user.js
+++ 14-user-task/user.js
@@ -1,8 +1,6 @@
-import { Task } from './task.js';
-
 export class User {
     constructor(task) {
-        this.task = new Task(task.task);
+        this.task = task;
     }
     do() {
         this.task.run();
