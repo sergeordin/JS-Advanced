90b46f85300d38ba3784c64e5204c93e52ae4261
Sergei Ordin <seryogaordin@gmail.com>
2023-04-13 18:47:36 +0300

    затык

--- 13-buttons/app.js
+++ 13-buttons/app.js
@@ -1,21 +1,15 @@
 const parent = document.querySelector('.buttons');
-const buttons = parent.querySelectorAll('.btn');
 const counter = document.querySelector('.counter');
 const span = parent.querySelectorAll('.span');
 
 parent.addEventListener('click', (event) => {
-    if (
-        event.target.classList.contains('btn') ||
-        event.target.classList.contains('span')
-    ) {
-        for (let i = 0; i < span.length; i++) {
-            if (span[i].closest('button').classList.contains('btn-red')) {
-                span[i].closest('button').classList.remove('btn-red');
-                span[i].innerHTML = 'Нажми меня';
-            }
-        }
-        event.target.closest('button').classList.add('btn-red');
-        event.target.innerHTML = 'Нажата!';
-        counter.innerHTML = Number(counter.innerHTML) + 1;
+    for (let i = 0; i < span.length; i++) {
+        span[i].closest('button').classList.remove('btn-red');
+        span[i].innerHTML = 'Нажми меня';
     }
+    event.target.closest('button').classList.add('btn-red');
+    event.target.closest('span').innerHTML = 'Нажата';
+
+    //event.target.innerHTML = 'Нажата';
+    counter.innerHTML = Number(counter.innerHTML) + 1;
 });
