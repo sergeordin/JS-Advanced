aa0ce88742127d3348c0e1f25a7d9548d2c991a4
Sergei Ordin <seryogaordin@gmail.com>
2023-04-13 10:18:27 +0300

    намудрил

--- 13-buttons/app.js
+++ 13-buttons/app.js
@@ -1,14 +1,20 @@
 const parent = document.querySelector('.buttons');
 const buttons = parent.querySelectorAll('.btn');
 const counter = document.querySelector('.counter');
+const span = parent.querySelectorAll('.span');
 
 parent.addEventListener('click', (event) => {
-    if (event.target.classList.contains('btn')) {
-        for (let i = 0; i < buttons.length; i++) {
-            buttons[i].classList.remove('btn-red');
-            buttons[i].innerHTML = 'Нажми меня';
+    if (
+        event.target.classList.contains('btn') ||
+        event.target.classList.contains('span')
+    ) {
+        for (let i = 0; i < span.length; i++) {
+            if (span[i].closest('button').classList.contains('btn-red')) {
+                span[i].closest('button').classList.remove('btn-red');
+                span[i].innerHTML = 'Нажми меня';
+            }
         }
-        event.target.classList.add('btn-red');
+        event.target.closest('button').classList.add('btn-red');
         event.target.innerHTML = 'Нажата!';
         counter.innerHTML = Number(counter.innerHTML) + 1;
     }
