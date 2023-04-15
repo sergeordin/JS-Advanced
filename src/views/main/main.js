ca8d0420d1d4456e15b7c5247e0ed830a375dbd1
Sergei Ordin <seryogaordin@gmail.com>
2023-04-15 19:38:46 +0300

    Search logics

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -14,19 +14,39 @@ export class MainView extends AbstractView {
         super();
         this.appState = appState;
         this.appState = onChange(this.appState, this.appStateHook.bind(this));
+        this.state = onChange(this.state, this.stateHook.bind(this));
         this.setTitle('Поиск книг');
     }
 
     appStateHook(path) {
-        console.log(path);
         if (path === 'favorites') {
             console.log(path);
         }
     }
 
+    async stateHook(path) {
+        if (path === 'searchQuery') {
+            this.state.loading = true;
+            const data = await this.loadList(
+                this.state.searchQuery,
+                this.state.offset
+            );
+            this.state.loading = false;
+            this.state.list = data.docs;
+            console.log(data.docs);
+        }
+    }
+
+    async loadList(q, offset) {
+        const res = await fetch(
+            `https://openlibrary.org/search.json?q=${q}&offset=${offset}`
+        );
+        return res.json();
+    }
+
     render() {
         const main = document.createElement('div');
-        main.append(new Search(this.appState).render());
+        main.append(new Search(this.state).render());
         this.app.innerHTML = '';
         this.app.append(main);
         this.renderHeader();
