8d134356fb224558f22d1701d07a945663226f13
Sergei Ordin <seryogaordin@gmail.com>
2023-04-16 14:42:19 +0300

    Card list (loading)

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -2,6 +2,7 @@ import { AbstractView } from '../../common/view.js';
 import onChange from 'on-change';
 import { Header } from '../../components/header/header.js';
 import { Search } from '../../components/search/search.js';
+import { CardList } from '../../components/card-list/card-list.js';
 
 export class MainView extends AbstractView {
     state = {
@@ -33,7 +34,9 @@ export class MainView extends AbstractView {
             );
             this.state.loading = false;
             this.state.list = data.docs;
-            console.log(data.docs);
+        }
+        if (path === 'list' || path === 'loading') {
+            this.render();
         }
     }
 
@@ -47,6 +50,7 @@ export class MainView extends AbstractView {
     render() {
         const main = document.createElement('div');
         main.append(new Search(this.state).render());
+        main.append(new CardList(this.appState, this.state).render());
         this.app.innerHTML = '';
         this.app.append(main);
         this.renderHeader();
