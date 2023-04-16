ca48d51aaa70ac68086380360065b800c812ff70
Sergei Ordin <seryogaordin@gmail.com>
2023-04-16 10:33:36 +0300

    fix

--- 13-buttons/app.js
+++ 13-buttons/app.js
@@ -1,15 +1,15 @@
 const parent = document.querySelector('.buttons');
 const counter = document.querySelector('.counter');
 const span = parent.querySelectorAll('.span');
+const btns = parent.querySelectorAll('.btn');
 
 parent.addEventListener('click', (event) => {
-    for (let i = 0; i < span.length; i++) {
-        span[i].closest('button').classList.remove('btn-red');
-        span[i].innerHTML = 'Нажми меня';
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].classList.remove('btn-red');
+        btns[i].innerHTML = 'Нажми меня';
     }
     event.target.closest('button').classList.add('btn-red');
-    event.target.closest(
-        'button'
-    ).innerHTML = `<span class="span">Нажата</span>`;
+    event.target.innerHTML = `<span class="span">Нажата</span>`;
+
     counter.innerHTML = Number(counter.innerHTML) + 1;
 });
