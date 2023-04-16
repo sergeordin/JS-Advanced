95fab1e385c5095c0eaed107aaa5131ef44cb24c
Sergei Ordin <seryogaordin@gmail.com>
2023-04-16 15:49:56 +0300

    fix

--- dist/app.js
+++ dist/app.js
@@ -1030,13 +1030,13 @@ class Header extends DivComponent {
         this.el.classList.add('header');
         this.el.innerHTML = `
 			<div>
-				<img src="/static/logo.svg" alt="Логотип" />
+				<img src="./static/logo.svg" alt="Логотип" />
 			</div>
             	<div class="menu">
 		<a class="menu__item" href="#">
-			<img src="/static/search.svg" alt="Поиск книг" />Поиск книг</a>
+			<img src="./static/search.svg" alt="Поиск книг" />Поиск книг</a>
 		<a class="menu__item" href="#favorites">
-			<img src="/static/favorites.svg" alt="Избранное" />Избранное
+			<img src="./static/favorites.svg" alt="Избранное" />Избранное
 			<div class="menu__counter">
 				${this.appState.favorites.length}
 			</div>
